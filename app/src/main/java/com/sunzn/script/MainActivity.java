92c14503845ec73333aa4b3984fcb61c1cc2fe2c
sunzn <sunzn.cn@gmail.com>
2018-04-20 18:48:33 +0800

    First Committt

---
+++ app/src/main/java/com/sunzn/script/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sunzn.script;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint({"JavascriptInterface", "AddJavascriptInterface", "SetJavaScriptEnabled"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView web = findViewById(R.id.web);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.addJavascriptInterface(new Object() {
+            @JavascriptInterface
+            public void send(String message) {
+                Log.e("MainActivity", message);
+            }
+        }, "androidObject");
+        web.loadUrl("file:///android_asset/demo.html");
+    }
+}
